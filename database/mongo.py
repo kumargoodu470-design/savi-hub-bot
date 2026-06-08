ff84ea8be2fe4b5c5f30a895c1c8a26572937e42
kumargoodu470-design <kumargoodu470@gmail.com>
2026-06-08 12:20:26 +0530

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -7,3 +7,22 @@ db = mongo_client["savihub"]
 
 files_collection = db["files"]
 users_collection = db["users"]
+async def add_file(title, quality, size, file_id):
+
+    await files_collection.insert_one(
+        {
+            "title": title.lower(),
+            "quality": quality,
+            "size": size,
+            "file_id": file_id
+        }
+    )
+
+
+async def get_files(title):
+
+    return await files_collection.find(
+        {
+            "title": title.lower()
+        }
+    ).to_list(length=50)
