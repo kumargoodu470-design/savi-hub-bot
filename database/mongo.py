faad3f2fea335283bb6dd121b7151c3d5b9bd65e
kumargoodu470-design <kumargoodu470@gmail.com>
2026-06-08 11:38:56 +0530

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -1,1 +1,9 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_URI
 
+mongo_client = AsyncIOMotorClient(MONGO_URI)
+
+db = mongo_client["savihub"]
+
+files_collection = db["files"]
+users_collection = db["users"]
