f196759fab88b1b1d2cc98343501a38c00c6c749
kumargoodu470-design <kumargoodu470@gmail.com>
2026-06-08 11:40:01 +0530

    Update forcejoin.py
    Added force join checker

--- database/handlers/handlers/forcejoin.py
+++ database/handlers/handlers/forcejoin.py
@@ -1,1 +1,18 @@
+from pyrogram.errors import UserNotParticipant
+from config import FORCE_CHANNEL
 
+async def check_force_join(client, user_id):
+
+    try:
+        await client.get_chat_member(
+            FORCE_CHANNEL,
+            user_id
+        )
+
+        return True
+
+    except UserNotParticipant:
+        return False
+
+    except:
+        return False
