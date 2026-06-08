9917403165f34edbeba8464e7d8f652ed0771c9e
kumargoodu470-design <kumargoodu470@gmail.com>
2026-06-08 11:41:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
+from pyrogram import Client
 
+from config import *
+
+app = Client(
+    "savihub",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+from handlers.start import *
+
+print("SAVI HUB Started")
+
+app.run()
